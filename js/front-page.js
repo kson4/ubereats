1f1f92104295a8c75e449bffdd28b98b6daa36ad
david <davidsinternetmail@gmail.com>
2022-10-16 12:54:35 -0700

    API call to leaflet

--- js/front-page.js
+++ js/front-page.js
@@ -6,49 +6,63 @@ const addressLogo = document.createElement("i");
 addressLogo.classList.add("fa-solid");
 addressLogo.classList.add("fa-location-dot");
 const addressList = document.querySelector(".address-suggestions");
+const key = "&apiKey=dbf5f4e5b664437a80b971c12ac03436";
+let timeout = null;
 
 address.addEventListener("keyup", (e) => {
-  if (address.value == "") {
-    addressList.style.visibility = "hidden";
-    console.log("asd");
-  } else {
-    addressList.style.visibility = "visible";
-  }
+  clearTimeout(timeout);
 
-  let allSuggestions = document.querySelectorAll(".address-suggestion");
-  allSuggestions.forEach((suggestion) => {
-    suggestion.remove();
-  });
+  let requestOptions = {
+    method: "GET",
+  };
+  timeout = setTimeout(() => {
+    let current = address.value;
+    fetch(
+      "https://api.geoapify.com/v1/geocode/autocomplete?text=" + current + key,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (address.value == "") {
+          addressList.style.visibility = "hidden";
+        } else {
+          addressList.style.visibility = "visible";
+        }
 
-  for (let i of cities) {
-    if (
-      i.toLowerCase().startsWith(address.value.toLowerCase()) &&
-      address.value != ""
-    ) {
-      const container = document.createElement("li");
-      container.classList.add("address-suggestion");
+        let allSuggestions = document.querySelectorAll(".address-suggestion");
+        allSuggestions.forEach((suggestion) => {
+          suggestion.remove();
+        });
 
-      const addressLogo = document.createElement("i");
-      addressLogo.classList.add("fa-solid");
-      addressLogo.classList.add("fa-location-dot");
-      addressLogo.classList.add("suggestion-logo");
+        console.log(result);
+        for (let i = 0; i < result.features.length; i++) {
+          console.log(current + " " + result.features[i].properties.formatted);
+          const container = document.createElement("li");
+          container.classList.add("address-suggestion");
 
-      let item = document.createElement("p");
-      item.classList.add("address-suggestion-text");
-      let word = i.substring(0, address.value.length);
-      word += i.substring(address.value.length);
-      item.textContent = word;
+          const addressLogo = document.createElement("i");
+          addressLogo.classList.add("fa-solid");
+          addressLogo.classList.add("fa-location-dot");
+          addressLogo.classList.add("suggestion-logo");
 
-      container.append(addressLogo);
-      container.append(item);
-      addressList.append(container);
-    }
-  }
-  // console.log(addressList.childElementCount);
-  if (addressList.childElementCount < 5 && addressList.childElementCount > 0) {
-    addressList.style.height = addressList.childElementCount * 75 + "px";
-    console.log(addressList.childElementCount * 75);
-  } else {
-    addressList.style.height = 375 + "px";
-  }
+          let item = document.createElement("p");
+          item.classList.add("address-suggestion-text");
+          item.textContent =
+            current + "\r\n" + result.features[i].properties.formatted;
+
+          container.append(addressLogo);
+          container.append(item);
+          addressList.append(container);
+        }
+        if (
+          addressList.childElementCount < 5 &&
+          addressList.childElementCount > 0
+        ) {
+          addressList.style.height = addressList.childElementCount * 75 + "px";
+        } else {
+          addressList.style.height = 375 + "px";
+        }
+      })
+      .catch((error) => console.log("error", error));
+  }, 1000);
 });
